0a3b77bea9af4ccd6dc1ed35e425ce4444bea819
Albin <albinanto06@gmail.com>
2024-05-21 11:35:15 +0530

    change redirecting path login page to /overview

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -38,13 +38,13 @@ console.log(userData,"dfghjkl;lkjhg")
   if (userData.role === 'operator'){
   
     //  router.push('/admin/operator/appointments')
-    return window.location.href ='/admin/overview'
+    return window.location.href ='/overview'
     
   }
 
    else if (userData.role === 'admin'){
   
-    return window.location.href ='/admin/overview'
+    return window.location.href ='/overview'
     
   }
   // else{
